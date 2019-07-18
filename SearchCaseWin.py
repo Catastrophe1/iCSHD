35626edf6df6ff31a43cf7f99b368cd3e5a2a791
Zhengjia Li <t-zhel@microsoft.com>
2019-07-18 17:05:01 +0800

    Show after searching

--- SearchCaseWin.py
+++ SearchCaseWin.py
@@ -4,7 +4,7 @@
 @Author: t-zhel
 @Date: 2019-07-13 23:05:14
 @LastEditTime: 2019-07-13 23:16:01
-@LastEditTime: 2019-07-16 23:49:41
+@LastEditTime: 2019-07-18 17:03:41
 @Description: file content
 '''
 
@@ -61,13 +61,21 @@ class SearchLineWidget(QWidget):
         case = cur.fetchall()[0]
 
         self.parent().caseAgeWidget.textEdit.setText(str(case[2]))
+        self.parent().caseAgeWidget.setVisible(True)
         self.parent().idleTimeWidget.textEdit.setText(str(case[3]))
+        self.parent().idleTimeWidget.setVisible(True)
         self.parent().serverityWidget.textEdit.setText(str(case[4]))
+        self.parent().serverityWidget.setVisible(True)
         self.parent().custSentimentalWidget.textEdit.setText(str(case[7]))
+        self.parent().custSentimentalWidget.setVisible(True)
         self.parent().productSupportedWidget.textEdit.setText(case[8])
+        self.parent().productSupportedWidget.setVisible(True)
         self.parent().engineerIDWidget.textEdit.setText(case[11])
+        self.parent().engineerIDWidget.setVisible(True)
         self.parent().isFDRWidget.textEdit.setText(str(case[12]))
+        self.parent().isFDRWidget.setVisible(True)
         self.parent().isResolvedWidget.textEdit.setText(str(case[13]))
+        self.parent().isResolvedWidget.setVisible(True)
 
 class SearchCaseWin(QWidget):
     def __init__(self, sqlcon, parent=None, **kwargs):
@@ -79,27 +87,35 @@ class SearchCaseWin(QWidget):
         vbox.addWidget(self.searchLineWidget)
 
         self.caseAgeWidget = LineWidget("Case Age")
+        self.caseAgeWidget.setVisible(False)
         vbox.addWidget(self.caseAgeWidget)
 
         self.idleTimeWidget = LineWidget("Idle Time")
+        self.idleTimeWidget.setVisible(False)
         vbox.addWidget(self.idleTimeWidget)
 
         self.serverityWidget = LineWidget("Serverity")
+        self.serverityWidget.setVisible(False)
         vbox.addWidget(self.serverityWidget)
 
         self.custSentimentalWidget = LineWidget("Customer Sentimental")
+        self.custSentimentalWidget.setVisible(False)
         vbox.addWidget(self.custSentimentalWidget)
 
         self.productSupportedWidget = LineWidget("Product Supported")
+        self.productSupportedWidget.setVisible(False)
         vbox.addWidget(self.productSupportedWidget)
 
         self.engineerIDWidget = LineWidget("Engineer ID")
+        self.engineerIDWidget.setVisible(False)
         vbox.addWidget(self.engineerIDWidget)
 
         self.isFDRWidget = LineWidget("isFDR")
+        self.isFDRWidget.setVisible(False)
         vbox.addWidget(self.isFDRWidget)
 
         self.isResolvedWidget = LineWidget("isResolved")
+        self.isResolvedWidget.setVisible(False)
         vbox.addWidget(self.isResolvedWidget)
 
         self.setLayout(vbox)
